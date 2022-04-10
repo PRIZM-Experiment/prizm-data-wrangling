ad46900566ec9c49c814540fccb90f27b8cfc5d3
fernandozhs <fernandozhs@gmail.com>
2022-04-09 21:31:24 -0400

    Fixed a bug in the `count` function.

--- metadatabase.py
+++ metadatabase.py
@@ -70,6 +70,7 @@ def count(categories=['Antenna', 'Switch', 'Temperature'], instruments=['100MHz'
                  "       END "
                  "       AS file_alias, "
                  "       DataTypes.data_type AS data_type, "
+                 "       DataDirectories.data_directory "
                  "FROM   DataDirectories "
                  "JOIN   DataCategories "
                  "ON     DataDirectories.data_category = DataCategories.data_category "
