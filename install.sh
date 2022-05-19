8e62992f2dce03c22ea171d5ad4a14ab1e105321
fernandozhs <fernandozhs@gmail.com>
2022-05-19 11:39:06 -0400

    Improved the installation script.
    The installation script now checks whether the file 'metadatabase.db'
    already exists in the respository's directory. If the file is present,
    it is deleted. This simple modification prevents conflicts when SQLite
    attempts to build and populate tables during installation.

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# Deletes the any previously generated PRIZM metadatabase left in the repository's directory.
+if [ -f metadatabase.db ] ; then
+    rm metadatabase.db
+fi
+
 # Builds and populates the PRIZM metadatabase in one fell swoop.
 sqlite3 < ./source/build.sql
