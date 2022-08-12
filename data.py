5ab051d013135345286facf74bdffe8aa8201197
fernandozhs <fernandozhs@gmail.com>
2022-08-12 11:06:22 -0400

    Added an important disclaimer in the docstring of the `from_directories` constructor.

--- data.py
+++ data.py
@@ -29,7 +29,11 @@ class Data(collections.UserDict):
 
     @classmethod
     def from_directories(cls, directory_addresses=['~'], classification_catalogue={'data_70MHz': '70MHz', 'data_100MHz': '100MHz'}, file_catalogue={'pol0.scio': ('float',['EW'],'pol'), 'pol1.scio': ('float',['NS'],'pol'), 'pol0.scio.bz2': ('float',['EW'],'pol'), 'pol1.scio.bz2': ('float',['NS'],'pol'), 'time_sys_stop.raw': ('float',['EW','NS'],'time_sys_stop'), 'time_sys_stop.raw': ('float',['EW','NS'],'time_sys_stop'), 'open.scio': ('float',['Switch'],'open'), 'short.scio': ('float',['Switch'],'short'), 'noise.scio': ('float',['Switch'],'noise'), 'res50.scio': ('float',['Switch'],'res50'), 'res100.scio': ('float',['Switch'],'res100'), 'antenna.scio': ('float',['Switch'],'antenna'), 'open.scio.bz2': ('float',['Switch'],'open'), 'short.scio.bz2': ('float',['Switch'],'short'), 'noise.scio.bz2': ('float',['Switch'],'noise'), 'res50.scio.bz2': ('float',['Switch'],'res50'), 'res100.scio.bz2': ('float',['Switch'],'res100'), 'antenna.scio.bz2': ('float',['Switch'],'antenna')}):
-        """ Loads the cataloged files located under the input directory addresses, organizing them according to the input classification catalogue. """
+        """ Loads the cataloged files located under the input directory addresses, organizing them according to the input classification catalogue.
+
+            Important: This constructor assumes that the data directory structure is headed by an instrument directory (e.g. '../data_70MHz') containing sub-directories for that instrument's antenna data (e.g. '../data_70MHz/16348', etc.), switch data (e.g. '../data_70MHz/switch'), and temperature data (e.g. '../data_70MHz/temperature'). 
+
+        """
 
         # Initializes the result sets.
         count_result_set = []
