d5f2f8390026c48b17134b2995725370d7dce554
Ronniy Joseph <ronniy.joseph@mcgill.ca>
2022-08-17 14:50:18 -0400

    Added a setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="prizmdatawrangling",
+    version='1.0',
+    author='Fernando Zago',
+    packages=['prizmdatawrangling'],
+    python_requires='>=3.6',
+    install_requires=[
+        "numpy >= 1.2",
+        "scipy >= 1.7",
+        "healpy >= 1.0",
+        "pygdsm >= 1.2",
+        "pbio >= 0.0.2"
+    ],
+)
