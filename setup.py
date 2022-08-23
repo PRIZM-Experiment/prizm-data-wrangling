ccfee7b66709714c4889e763f6798c44aae3b7c6
Ronniy Joseph <ronniy.joseph@mcgill.ca>
2022-08-23 15:51:15 -0400

    Fixed copying of settings.json to site package

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0',
     author='Fernando Zago',
     packages=['prizmdatawrangling'],
+    package_data={'prizmdatawrangling': ['settings.json']},
 
     python_requires='>=3.6',
     install_requires=[
